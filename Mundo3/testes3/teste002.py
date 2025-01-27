87b14574c8a715451ce2b53c3e3879adc95ce9a8
Kyk <contatokaiky.sf21@gmail.com>
2025-01-27 18:13:59 -0300

    Update teste002.py

--- Mundo3/testes3/teste002.py
+++ Mundo3/testes3/teste002.py
@@ -1,40 +1,10 @@
+# Testando Variável composta(LISTA)
 a = [2, 3, 4, 7]
 b = a[:]
 b[2] = 8
 
 print(f'Lista A: {a}')
 print(f'Lista B: {b}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
