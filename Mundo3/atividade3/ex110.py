144f5bebff1d34d2d551cfa899380b9869159614
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-02-08 20:54:12 -0300

    Modification in exercises

--- Mundo3/atividade3/ex110.py
+++ Mundo3/atividade3/ex110.py
@@ -1,4 +1,6 @@
+# Programa que utiliza pacote useful, para metade, dobro, aumento e redução de um valor determinado pelo o úsuario
 from useful import moeda
 
 valor = float(input('Digite o preço: R$'))
 moeda.resumo(valor, 20, 12)
+# Desenvolvido por Kaiky - 2025
--- Mundo3/atividade3/ex112.py
+++ Mundo3/atividade3/ex112.py
@@ -1,5 +1,7 @@
+# Programa que utiliza pacote useful, para metade, dobro, aumento e redução de um valor determinado pelo o úsuario, com um validador de número.
 from useful import moeda, dado
 
 n = dado.leitor_de_dinheiro('Digite o preço: R$\033[32m')
 print('\033[m')
 moeda.resumo(n, 35, 22)
+# Desenvolvido por Kaiky - 2025
